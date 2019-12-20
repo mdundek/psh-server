0097e1c6b987e5b526cfe1258f654997898896ee
Michael Dundek <mdundek@gmail.com>
2019-12-20 18:31:02 -0500

    minor refactoring of labels

--- client/psh-admin/service-worker.js
+++ client/psh-admin/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/psh-admin/precache-manifest.150645351281c031550d883b45e6b3c6.js"
+  "/psh-admin/precache-manifest.90e81945ce763884d47b44d35c2f5199.js"
 );
 
 workbox.clientsClaim();
--- resources/populateDockerHostIp.sh
+++ resources/populateDockerHostIp.sh
@@ -2,6 +2,26 @@
 
 # Sets the DOCKERHOST env variable for docker-compose .env file (not the .env of PSH server)
 
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    # If available, use LSB to identify distribution
+    if [ -f /etc/lsb-release -o -d /etc/lsb-release.d ]; then
+        export DISTRO=$(lsb_release -i | cut -d: -f2 | sed s/'^\t'//)
+    # Otherwise, use release info file
+    else
+        export DISTRO=$(ls -d /etc/[A-Za-z]*[_-][rv]e[lr]* | grep -v "lsb" | cut -d'/' -f3 | cut -d'-' -f1 | cut -d'_' -f1 | tr "\n" " ")
+    fi
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    export DISTRO="OSX"
+else
+    echo "Unsupported operating system: $OSTYPE"
+    exit 1
+fi
+
 cd "$1"
 rm -rf .env
-echo 'DOCKERHOST='$(ip -4 addr show scope global dev docker0 | grep inet | awk '{print $2}' | cut -d / -f 1) > .env
+
+if [ "$DISTRO" == "Ubuntu" ]; then
+    echo 'DOCKERHOST='$(ip -4 addr show scope global dev docker0 | grep inet | awk '{print $2}' | cut -d / -f 1) > .env
+elif [[ "$DISTRO" == *"redhat"*] || ["$DISTRO" == *"centos"* ]]; then
+    echo 'DOCKERHOST='$(ip -4 addr show scope global dev docker0 | grep inet | awk '{print $2}' | cut -d / -f 1) > .env
+fi
