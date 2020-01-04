c3015720f2cf8d891af5a08cc0385a17f1ced7b0
Michael Dundek <mdundek@gmail.com>
2020-01-04 18:22:54 +0100

    Debugging

--- client/psh-admin/precache-manifest.e775036ff19b859260ef6ecca2892b0b.js
+++ client/psh-admin/precache-manifest.e775036ff19b859260ef6ecca2892b0b.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/psh-admin/static/js/runtime~main.78d9c6be.js"
   },
   {
-    "revision": "ca125b543bfd4cb8de9c",
-    "url": "/psh-admin/static/js/main.ca125b54.chunk.js"
+    "revision": "43aac6e622789a8a0b4d",
+    "url": "/psh-admin/static/js/main.43aac6e6.chunk.js"
   },
   {
     "revision": "4d2c3440def7bd5392c7",
     "url": "/psh-admin/static/js/1.4d2c3440.chunk.js"
   },
   {
-    "revision": "ca125b543bfd4cb8de9c",
+    "revision": "43aac6e622789a8a0b4d",
     "url": "/psh-admin/static/css/main.bd7ca0ca.chunk.css"
   },
   {
-    "revision": "3174f8f5c1ef159008725513b645b736",
+    "revision": "5e17046d9bd9c21ea7d98e593736677f",
     "url": "/psh-admin/index.html"
   }
 ];
--- client/psh-admin/service-worker.js
+++ client/psh-admin/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/psh-admin/precache-manifest.90e81945ce763884d47b44d35c2f5199.js"
+  "/psh-admin/precache-manifest.e775036ff19b859260ef6ecca2892b0b.js"
 );
 
 workbox.clientsClaim();
